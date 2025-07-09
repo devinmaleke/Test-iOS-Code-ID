86a238e0b04019b9851e7dbfd528542ba4f611c9
Devin <devin.maleke@gmail.com>
2025-07-09 20:47:05 +0700

    first commit

---
+++ Test_Code_ID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Code_ID
+//
+//  Created by Devin Maleke on 09/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_Code_IDTests/Test_Code_IDTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Code_IDTests.swift
+//  Test_Code_IDTests
+//
+//  Created by Devin Maleke on 09/07/25.
+//
+
+import Testing
+@testable import Test_Code_ID
+
+struct Test_Code_IDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
