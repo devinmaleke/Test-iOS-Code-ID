be3a048a52f32c7395917ddc5dcdc2a372072e18
Devin <devin.maleke@gmail.com>
2025-07-09 23:14:14 +0700

    feat: Slicing Login and Register

---
+++ Test_Code_ID/Other Resources/Functions.swift
@@ -0,0 +1,8 @@
+//
+//  Functions.swift
+//  Test_Code_ID
+//
+//  Created by Devin Maleke on 09/07/25.
+//
+
+import Foundation
---
+++ Test_Code_ID/View/Tab Bar/TabBarVC.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarVC.swift
+//  Test_Code_ID
+//
+//  Created by Devin Maleke on 09/07/25.
+//
+
+import Foundation
