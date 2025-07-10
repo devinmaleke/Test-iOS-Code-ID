b8ef6651d9889e04442262dff6fd749229e99363
Devin <devin.maleke@gmail.com>
2025-07-11 00:39:51 +0700

    feat: Slicing Home and Detail Home

---
+++ Test_Code_ID/View/Home Detail/HomeDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeDetailVC.swift
+//  Test_Code_ID
+//
+//  Created by Devin Maleke on 11/07/25.
+//
+
+import UIKit
+
+class HomeDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Test_Code_ID/View/Home/Content/HomeListTVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeListTVC.swift
+//  Test_Code_ID
+//
+//  Created by Samir iOS on 10/07/25.
+//
+
+import UIKit
+
+class HomeListTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
